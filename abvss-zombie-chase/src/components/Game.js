1a29e00b61d9ac164d8227701b5b1f92933d74d9
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-11-16 13:48:29 +0300

    done

--- abvss-zombie-chase/src/components/Game.js
+++ abvss-zombie-chase/src/components/Game.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Player from './Player';
 import Zombie from './Zombie';
-import './Game.css';
+import './app.css';
 
 const Game = () => {
   const [playerPosition, setPlayerPosition] = useState({ x: 50, y: 0 });
   const [zombiePosition, setZombiePosition] = useState({ x: 200, y: 0 });
   const [score, setScore] = useState(0);
 
-  const handleJump = () => {
-    setPlayerPosition((prev) => ({ ...prev, y: 100 }));
-  };
+  const handleJump = useCallback(() => {
+    if (playerPosition.y === 0) {
+      setPlayerPosition((prev) => ({ ...prev, y: 100 }));
+    }
+  }, [playerPosition.y]);
 
-  const handleCollision = () => {
-    // Implement collision detection logic
-  };
+  const updateGame = useCallback(() => {
+    setPlayerPosition((prev) => {
+      if (prev.y > 0) {
+        // Simulate gravity
+        return { ...prev, y: Math.max(0, prev.y - 5) };
+      }
+      return prev;
+    });
 
-  const updateGame = () => {
-    // Implement game update logic (e.g., move zombie, check for collisions)
-  };
+    setZombiePosition((prev) => ({ ...prev, x: prev.x - 2 }));
+
+    // Check for collision
+    if (
+      playerPosition.x < zombiePosition.x + 40 &&
+      playerPosition.x + 40 > zombiePosition.x &&
+      playerPosition.y < zombiePosition.y + 40 &&
+      playerPosition.y + 40 > zombiePosition.y
+    ) {
+      // Collision occurred, handle accordingly (e.g., end game)
+      console.log('Collision!');
+    }
+
+    // Check if zombie passed the player
+    if (zombiePosition.x < 0) {
+      setZombiePosition({ x: 400, y: 0 });
+      setScore((prev) => prev + 1);
+    }
+  }, [playerPosition.x, playerPosition.y, zombiePosition]);
 
   useEffect(() => {
-    // Set up game loop
     const gameLoop = setInterval(() => {
       updateGame();
-    }, 1000 / 60); // 60 frames per second
+    }, 1000 / 60);
 
-    // Clean up the interval on component unmount
     return () => clearInterval(gameLoop);
-  }, []);
+  }, [updateGame]);
 
   return (
     <div className="game-container">
